114b8bd12f28a829718577ad8ae359efb9a2f7a3
AT <agnieszkatarsa@gmail.com>
2021-05-10 20:25:27 +0100

    Upade `IconInput` component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,77 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+  const styles = STYLES[size];
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+
+      <TextInput
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height + 'px',
+          '--border-thickness': styles.borderThickness + 'px',
+          '--font-size': styles.fontSize + 'px',
+        }}
+        {...delegated}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+  color: inherit;
+`;
+
+const TextInput = styled.input`
+  height: var(--height);
+  width: var(--width);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-size: var(--font-size);
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
 
 export default IconInput;
